93ce2d56fb23067053777940b7fa09ea6de00d6e
kpling <harrelchris@gmail.com>
2019-09-17 22:05:16 -0500

    Add rest_framework, swagger, and api

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
